e996f6d8d4bb51fbaade7071f1b0787e4ff59a5b
Michel Heon <heon.michel@uqam.ca>
2021-04-29 05:30:49 -0400

    intégration et configuratio openjdk, tomcat et solr

---
+++ features/ca.uqam.tool.external.solr.feature/archive/get_solr.sh
@@ -0,0 +1,11 @@
+#!/bin/bash 
+
+###################################################################
+# Script Name   :
+# Description   :
+# Args          : 
+# Author       	: Michel Héon	
+# Email         : heon.michel@uqam.ca
+###################################################################
+wget https://archive.apache.org/dist/lucene/solr/7.7.2/solr-7.7.2.tgz
+tar xzf solr-7.7.2.tgz
